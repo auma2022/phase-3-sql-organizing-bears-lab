1ff20495d4ca81a0f750ad3fffc4bfc42520dfec
auma2022 <auma.amwayi@student.moringaschool.com>
2022-08-23 23:25:53 +0300

    commit tree b486fae14deabd28d9ac9ec866ac2b3f2af41cc8

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (id,name,age,sex,color,temperament,alive) VALUES (1,"Mr. Chocolate",3,"male","white","angry",true);
+INSERT INTO bears (id,name,age,sex,color,temperament,alive) VALUES (2,"Rowdy",6,"male","white","angry",true);
+INSERT INTO bears (id,name,age,sex,color,temperament,alive) VALUES(3,"Tabitha",3,"female","black","angry",true);
+INSERT INTO bears (id,name,age,sex,color,temperament,alive) VALUES(4,"Sergent Brown", 3,"male","brown","angry",true); 
+INSERT INTO bears (id,name,age,sex,color,temperament,alive) VALUES(5,"Melisa",3,"male","white","angry",true);
+INSERT INTO bears (id,name,age,sex,color,temperament,alive) VALUES(6,"Grinch",3,"male","grey","happy",false);
+INSERT INTO bears (id,name,age,sex,color,temperament,alive) VALUES(7,"Wendy",3,"male","brown","happy",true);
+INSERT INTO bears (id,name,age,sex,color,temperament,alive) VALUES (8,NULL,3,"male","brown","angry",false);
